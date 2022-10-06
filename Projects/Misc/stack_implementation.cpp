2aad025c1b8d94aeb3d9dead3d5a4e934e178ef5
Yokohama <dimensionalbleedthrough48@gmail.com>
2022-10-06 22:14:40 +0800

    Added stack_implementation.cpp in Misc directory

---
+++ Projects/Misc/stack_implementation.cpp
@@ -0,0 +1,23 @@
+// https://www.geeksforgeeks.org/introduction-to-stack-data-structure-and-algorithm-tutorials/
+
+// TBD
+#include <bits/stdc++.h>
+
+#define MAX 1000
+
+class Stack {
+    int top;
+
+public:
+    int a = MAX;    // maximum size of stack
+    // constructor
+    Stack() {
+        top = -1;
+    }
+
+    bool push(int x);
+    int pop();
+    int peek();
+    bool isEmpty();
+
+};
