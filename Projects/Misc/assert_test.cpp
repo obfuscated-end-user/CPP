43038aa7f7acaf11bda488ddddfa21e9c27d7a66
Yokohama <dimensionalbleedthrough48@gmail.com>
2022-11-06 23:21:12 +0800

    added assert_test.cpp

---
+++ Projects/Misc/assert_test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cassert>
+
+int main() {
+    /* int even_num = 3;
+
+    // asserts the value of even_num must be even
+    assert((even_num % 2 == 0)); */
+
+    const int number = 1;
+    /* std::cout << "Enter a number: ";
+    std::cin >> number; */
+    static_assert((number % 0), "Number is not even"); FUCK FUCK FUCK
+    return 0;
+}
