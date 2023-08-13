6d1ed5edb632024d968a8bd013b88755abfd2d3d
obfuscated-end-user <dimensionalbleedthrough48@gmail.com>
2023-08-13 18:24:28 +0800

    Added 17 directory

---
+++ Projects/LearnCPP/17/17_2/inheritance.cpp
@@ -0,0 +1,60 @@
+// https://www.learncpp.com/cpp-tutorial/basic-inheritance-in-c
+
+#include <iostream>
+#include <string>
+#include <string_view>
+
+class Person {
+public:
+    std::string m_name;
+    int m_age;
+
+    Person(std::string_view name="", int age=0): m_name{name}, m_age{age} {}
+
+    const std::string& getName() const {
+        return m_name;
+    }
+    int getAge() const {
+        return m_age;
+    }
+};
+
+// BaseballPlayer publicly inheriting Person
+class BaseballPlayer : public Person {
+public:
+    double m_battingAverage;
+    int m_homeRuns;
+
+    BaseballPlayer(double battingAverage=0.0, int homeRuns=0): m_battingAverage{battingAverage}, m_homeRuns{homeRuns} {}
+};
+
+// Employee publicly inherits from Person
+class Employee: public Person {
+public:
+    double m_hourlySalary;
+    long m_employeeID;
+
+    Employee(double hourlySalary=0.0, long employeeID=0): m_hourlySalary{hourlySalary}, m_employeeID{employeeID} {}
+
+    void printNameAndSalary() const {
+        std::cout << m_name << ": " << m_hourlySalary << '\n';
+    }
+};
+
+// inherits both Employee and Person
+class Supervisor: public Employee {
+public:
+    long m_overseesIDs[5];
+};
+
+int main() {
+    BaseballPlayer mark {};
+    mark.m_name = "Mark"; // remember, m_name is public
+    std::cout << mark.getName() << '\n'; // use the getName() function we've acquired from the Person base class
+
+    Employee frank {20.25, 12345};
+    frank.m_name = "Frank";
+    frank.printNameAndSalary();
+
+    return 0;
+}
---
+++ Projects/LearnCPP/17/17_4/derived_init.cpp
@@ -0,0 +1,36 @@
+// https://www.learncpp.com/cpp-tutorial/constructors-and-initialization-of-derived-classes
+
+#include <iostream>
+
+class Base {
+private:
+    int m_id;
+
+public:
+    Base(int id=0): m_id{id} {}
+
+    int getId() const {
+        return m_id;
+    }
+};
+
+class Derived: public Base {
+private:
+    double m_cost;
+
+public:
+    // Call Base(int) constructor with value id
+    Derived(double cost=0.0, int id=0): Base{id}, m_cost{cost} {}
+
+    double getCost() const {
+        return m_cost;
+    }
+};
+
+int main() {
+    Derived derived { 1.3, 5 }; // use Derived(double, int) constructor
+    std::cout << "Id: " << derived.getId() << '\n';
+    std::cout << "Cost: " << derived.getCost() << '\n';
+
+    return 0;
+}
