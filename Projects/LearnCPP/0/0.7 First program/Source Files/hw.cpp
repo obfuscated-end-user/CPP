3eff0d4a4d132f358833e6fd21e855751cbb2306
obfuscated-end-user <dimensionalbleedthrough48@gmail.com>
2024-06-10 15:18:00 +0800

    Made some more minor adjustments.

--- Projects/LearnCPP/0/0.7 First program/Source Files/hw.cpp
+++ Projects/LearnCPP/0/0.7 First program/Source Files/hw.cpp
@@ -1,14 +1,14 @@
 // https://softwareengineering.stackexchange.com/questions/86914/whats-the-best-structure-for-a-repository
 // https://softwareengineering.stackexchange.com/questions/81899/how-should-i-organize-my-source-tree
-// https://mitcommlab.mit.edu/broad/commkit/file-structure/
+// https://mitcommlab.mit.edu/broad/commkit/file-structure
 // https://www.google.com/search?client=firefox-b-d&q=src+lib+doc+res+build+test
 // https://www.google.com/search?client=firefox-b-d&q=general+project+folder+structure+programming
 // https://www.google.com/search?client=firefox-b-d&q=vs+code+build+target
 
-// https://www.learncpp.com/
-// https://www.learncpp.com/cpp-tutorial/configuring-your-compiler-build-configurations/
+// https://www.learncpp.com
+// https://www.learncpp.com/cpp-tutorial/configuring-your-compiler-build-configurations
 
-# include <iostream>
+#include <iostream>
 
 int main() {
     std::cout << "Hello World!" << std::endl;
@@ -16,6 +16,6 @@ int main() {
     for (int i = 0; i < 10; i++) {
         std::cout << i << std::endl;
     }
-    std::cin.get(); // guarantees that the window will remain open until dumb fucks like you enters something
+    std::cin.get(); // guarantees that the window will remain open until you enter something
     return 0;
 }
