95bd4e0e2a6376f8145e3c3b9d43fb7e43f6d7ac
obfuscated-end-user <dimensionalbleedthrough48@gmail.com>
2025-01-07 15:27:15 +0800

    length_capacity.cpp and 16_11 (2025-01-07) dir

---
+++ Projects/LearnCPP/16/16_11 (2025-01-07)/push_emplace.cpp
@@ -0,0 +1,38 @@
+// https://www.learncpp.com/cpp-tutorial/stdvector-and-stack-behavior
+
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <vector>
+
+class Foo {
+private:
+    std::string m_a {};
+    int m_b {};
+
+public:
+    Foo(std::string_view a, int b)
+        : m_a { a }, m_b { b } {}
+    
+    explicit Foo(int b)
+        : m_a {}, m_b { b } {};
+};
+
+int main() {
+    std::vector<Foo> stack {};
+
+    // When we already have an object, push_back and emplace_back are similar in efficiency
+    Foo f { "a", 2 };
+    stack.push_back(f);
+    stack.emplace_back(f);
+
+    // When we need to create a temporary object to push, emplace back is more efficient
+    stack.push_back({ "a", 2 });    // creates a temporary object, and then copies it into the vector
+    stack.emplace_back("a", 2);     // forwards the arguments so the object can be created directly in the vector (no copy made)
+
+    // push_back won't use explicit constructors, emplace_back will
+    // stack.push_back({ 2 }); // compile error: Foo(int) is explicit
+    stack.emplace_back(2);  // ok
+
+    return 0;
+}
